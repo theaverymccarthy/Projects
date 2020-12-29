1994936c46e5422cc2aff41aad31647a6b6dfea9
Avery McCarthy <69693126+theaverymccarthy@users.noreply.github.com>
2020-12-28 22:24:36 -0800

    settings

--- craigslistSeach.py
+++ craigslistSeach.py
@@ -1,5 +1,5 @@
 import selenium
 from selenium import webdriver
-driver = webdriver.Firefox(executable_path=r'/Users/averymccarthy/Documents/GitHub/projects/geckodriver.exe')
+driver = webdriver.Firefox()
 
 driver.get("https://selenium.dev")
