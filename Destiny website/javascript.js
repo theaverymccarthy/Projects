7a93db3ea38e8c6ea75f6a74762d460ffd6ca001
Avery McCarthy <69693126+theaverymccarthy@users.noreply.github.com>
2021-04-25 12:31:15 -0400

    setup

---
+++ Destiny website/javascript.js
@@ -0,0 +1,99 @@
+function zoom() {
+    var x = document.getElementById("Zoom");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function pvd() {
+    var x = document.getElementById("PVD");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function quarentine() {
+    var x = document.getElementById("Quarentine");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function colaborative() {
+    var x = document.getElementById("Colaborative");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function oneFish() {
+    var x = document.getElementById("OneFish");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function click() {
+    var x = document.getElementById("Click");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function faer() {
+    var x = document.getElementById("Faer");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function riso() {
+    var x = document.getElementById("RISO");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
+function abram() {
+    var x = document.getElementById("Abram");
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+}
