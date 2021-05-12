0062f9b01cedf2edae94e82f6732c58865403c33
Avery McCarthy <69693126+theaverymccarthy@users.noreply.github.com>
2021-05-11 22:25:57 -0400

    package management

--- Craigsearchbot/craigslistSeach.py
+++ Craigsearchbot/craigslistSeach.py
@@ -1,15 +1,23 @@
 from os import link
 import pytest
 from selenium import webdriver
-import sys
 from selenium.webdriver.common import service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import csv
 import pandas as pd
+import tkinter as tk
 
+window = tk.Tk()
+label = tk.Label(text="What items do you want?")
+entry = tk.Entry()
+label.pack()
+entry.pack()
 
+item = entry.get()
+window.mainloop()
+print (item)
 
 wantFree = input("Want free? Y/N ")
 whatWant = input("What you want? ")
@@ -26,6 +34,15 @@ if wantFree == "Y":
         sendClick = browser.find_element_by_class_name("searchbtn")
         sendClick.click()
 
+        ulSelector = browser.find_element_by_class_name("rows")
+        liClass = ulSelector.find_elements_by_tag_name("li")
+        with open('F:\Documents\projects\Craigsearchbot\craigsheet.csv', 'w', newline='') as file:
+                for links in liClass:
+                        linkyLinks = [links.find_element_by_xpath('./a').get_attribute('href')]
+                        print(linkyLinks)
+                        
+                        mywriter = csv.writer(file, delimiter=' ')
+                        mywriter.writerows([linkyLinks])
         
 
 else:
@@ -41,10 +58,10 @@ else:
 
         ulSelector = browser.find_element_by_class_name("rows")
         liClass = ulSelector.find_elements_by_tag_name("li")
-        with open('craigsheet.csv', 'w', newline='') as file:
+        with open('F:\Documents\projects\Craigsearchbot\craigsheet.csv', 'w', newline='') as file:
                 for links in liClass:
                         linkyLinks = [links.find_element_by_xpath('./a').get_attribute('href')]
                         print(linkyLinks)
                         
                         mywriter = csv.writer(file, delimiter=' ')
-                        mywriter.writerows(linkyLinks)
+                        mywriter.writerows([linkyLinks])
